4129fbb314a93a74c1ec8841ae35b84c615da011
Ritamnandy <nandyritam519@gmail.com>
2026-09-08 00:50:58 +0530

    modify auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import type { RedisService } from '../redis/redis.service.js';
+import type { MailService } from '../mail/mail.service.js';
 
 @Injectable()
-export class AuthService {}
+export class AuthService
+{
+    private readonly logger = new Logger(AuthService.name);
+    
+    constructor (
+        private readonly mailService: MailService,
+        private readonly redisService: RedisService ) { }
+}
