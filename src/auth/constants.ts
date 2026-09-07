187cb537a0779200f105170f29505ee6c5759269
Ritamnandy <nandyritam519@gmail.com>
2026-09-07 23:52:05 +0530

    feat(auth): add bcrypt for password hashing and comparison, implement OTP generation and token management

---
+++ src/auth/constants.ts
@@ -0,0 +1,59 @@
+
+import crypto from 'node:crypto'
+import bcrypt from 'bcrypt'
+
+const getOtp = (): string =>
+{
+    return crypto.randomInt( 100000, 999999 ).toString()
+}
+
+async function hashPassword ( password: string ): Promise<string>
+{
+    return await bcrypt.hash( password, 13 )
+}
+
+async function comparePassword ( password: string, hashPassword: string ): Promise<boolean>
+{
+    return bcrypt.compare( password, hashPassword )
+}
+
+const signupKey = ( email: string ) =>
+{
+    return `signup-key:${ email }`;
+};
+
+const otpKey = ( email: string ) =>
+{
+    return `otp-key:${ email }`;
+};
+
+const hashToken = ( token: string ) =>
+{
+    return crypto.createHash( 'sha256' ).update( token ).digest( 'hex' )
+}
+const rawToken = () => crypto.randomBytes( 32 ).toString( 'hex' )
+
+
+const resetTokenKey = ( hashtoken: string ) =>
+{
+    return `reset-token:${ hashtoken }`;
+};
+
+const ResetPasswordLink = ( token: string, email: string ) =>
+{
+    return `${ process.env.FONTEND_RESET_PASSWORD_URL as string }?token=${ token }&email=${ encodeURIComponent( email ) }`;
+};
+
+export
+{ 
+    ResetPasswordLink,
+    comparePassword,
+    getOtp,
+    hashPassword,
+    hashToken,
+    rawToken,
+    resetTokenKey,
+    signupKey,
+    otpKey
+    
+}
