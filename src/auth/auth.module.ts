ddb382c79428ed1e7adf41871d824788866d0831
Ritamnandy <nandyritam519@gmail.com>
2026-09-08 23:32:08 +0530

    update 2 files and create 1 file

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service.js';
 import { AuthController } from './auth.controller.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { AuthRepository } from './aut.repository.js';
+import { AuthguardsGuard } from './authguards/authguards.guard.js';
 
 @Module( {
-  imports:[PrismaModule],
-  providers: [AuthService],
-  controllers: [AuthController]
-})
-export class AuthModule {}
+  imports: [ PrismaModule ],
+  providers: [ AuthService, JwtService, ConfigService, AuthRepository, AuthguardsGuard ],
+  controllers: [ AuthController ],
+  exports: [ AuthguardsGuard ]
+} )
+export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,13 +2,16 @@ import { HttpException, Injectable, Logger, UnauthorizedException } from '@nestj
 import { RedisService } from '../redis/redis.service.js';
 import { MailService } from '../mail/mail.service.js';
 import { RegisterDto } from './dto/register.dto.js';
-import { getOtp, otpKey, signupKey } from './constants.js';
+import { comparePassword, getOtp, hashPassword, otpKey, signupKey, rawToken, hashToken, ResetPasswordLink, resetTokenKey } from './constants.js';
 import { AuthRepository } from './aut.repository.js';
 import { ConfigService } from '@nestjs/config';
 import { JsonWebTokenError, JwtService, NotBeforeError, TokenExpiredError } from '@nestjs/jwt';
 import { VerifyEmail } from './dto/verifyEmail.dto.js';
 import type { JwtPayLoad, RefreshTokenPayload } from './types/payload.types.js';
 import { StringValue } from 'ms';
+import { ResendOtpDto } from './dto/resendotp.dto.js';
+import { LoginDto } from './dto/login.dto.js';
+import type { ResetPasswordDto } from './dto/resetPassword.dto.js';
 @Injectable()
 export class AuthService
 {
@@ -53,13 +56,14 @@ export class AuthService
         return { accessToken, refreshToken };
     }
 
-    private decodeRefreshToken ( refreshToken: string )
+    private async decodeRefreshToken ( refreshToken: string ): Promise<RefreshTokenPayload>
     {
         try
         {
-            return this.jwtService.verifyAsync( refreshToken, {
+            const response: RefreshTokenPayload = await this.jwtService.verifyAsync( refreshToken, {
                 secret: this.configService.getOrThrow<string>( 'REFRESH_TOKEN_SECRET' )
             } )
+            return response
         } catch ( error )
         {
             if ( error instanceof TokenExpiredError )
@@ -99,7 +103,7 @@ export class AuthService
 
     }
 
-    async resendOtpCode ( data: VerifyEmail )
+    async resendOtpCode ( data: ResendOtpDto )
     {
         const signupData = await this.redisService.get( signupKey( data.email ) )
         if ( !signupData )
@@ -117,5 +121,170 @@ export class AuthService
         }
     }
 
+    async verifyEmail ( data: VerifyEmail )
+    {
+        const cachedUser = await this.redisService.get( signupKey( data.email ) )
+        if ( !cachedUser )
+        {
+            throw new HttpException( 'Signup data not found or Register Session expired', 400 )
+        }
+        const otp = await this.redisService.get( otpKey( data.email ) )
+        if ( !otp )
+        {
+            throw new HttpException( 'OTP not found or OTP expired,Please request a new OTP', 400 )
+        }
+        if ( otp !== data.otp )
+        {
+            throw new HttpException( 'Invalid OTP,Please enter a valid OTP', 400 )
+        }
+        const user = JSON.parse( cachedUser ) as RegisterDto
+        const hashpassword = await hashPassword( user.password )
+        const createdUser = await this.authRepository.createUser( { ...user, password: hashpassword } )
+        const { jwt, refresh } = this.createPayLoad( {
+            id: createdUser.userId,
+            email: createdUser.email,
+            firstName: createdUser.firstName,
+            lastName: createdUser.lastName
+        } )
+        const { accessToken, refreshToken } = await this.genarateTokenPair( jwt, refresh )
+        await this.redisService.delete( signupKey( data.email ) )
+        await this.redisService.delete( otpKey( data.email ) )
+        this.logger.log( `User verified and created ${ data.email }` )
+        return {
+            message: 'User verified and created successfully',
+            user: createdUser,
+            accessToken: accessToken,
+            refreshToken: refreshToken
+        }
+    }
 
+    async loginUser ( data: LoginDto )
+    {
+        const existingUser = await this.authRepository.findWhenLogin( data.email )
+        if ( !existingUser )
+        {
+            throw new HttpException( 'User not found,Please register first', 404 )
+        }
+        const isPasswordCurrect = await comparePassword( data.password, existingUser.password )
+        if ( !isPasswordCurrect )
+        {
+            throw new HttpException( 'Invalid credentials,Please enter valid credentials', 400 )
+        }
+        const { jwt, refresh } = this.createPayLoad( {
+            id: existingUser.userId,
+            email: existingUser.email,
+            firstName: existingUser.firstName,
+            lastName: existingUser.lastName
+        } )
+        const { accessToken, refreshToken } = await this.genarateTokenPair( jwt, refresh )
+        this.logger.log( `User logged in ${ data.email }` )
+        return {
+            message: 'User logged in successfully',
+            user: {
+                userId: existingUser.userId,
+                email: existingUser.email,
+                firstName: existingUser.firstName,
+                lastName: existingUser.lastName,
+                createdAt: existingUser.createdAt,
+                updatedAt: existingUser.updatedAt
+            },
+            accessToken: accessToken,
+            refreshToken: refreshToken
+        }
+
+    }
+
+    async logoutUser ( userId: string )
+    {
+        const user = await this.authRepository.logOutUser( userId )
+        this.logger.log( `User logged out ${ user.email }` )
+        return {
+            message: 'User logged out successfully'
+        }
+    }
+
+    async refreshAccessToken ( refreshToken: string )
+    {
+        const decoded = await this.decodeRefreshToken( refreshToken )
+        const user = await this.authRepository.findWhenLogin( decoded.email )
+        if ( !user )
+        {
+            throw new HttpException( 'User not found', 404 )
+        }
+        if ( refreshToken !== user.refreshToken )
+        {
+            throw new HttpException( 'Invalid refresh token', 401 )
+        }
+        const { jwt, refresh } = this.createPayLoad( {
+            id: user.userId,
+            email: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName
+        } )
+        const { accessToken, refreshToken: newRefreshToken } = await this.genarateTokenPair( jwt, refresh )
+        this.logger.log( `User refreshed access token ${ user.email }` )
+        return {
+            message: 'User refreshed access token successfully',
+            accessToken: accessToken,
+            refreshToken: newRefreshToken
+        }
+    }
+
+    async forgotPassword ( data: ResendOtpDto )
+    {
+        const user = await this.authRepository.findByEmail( data.email )
+        if ( !user )
+        {
+            throw new HttpException( 'User not found', 404 )
+        }
+        const rowtoken = rawToken()
+        const hashedToken = hashToken( rowtoken )
+        const link = ResetPasswordLink( hashedToken, user.email )
+        await this.redisService.set( resetTokenKey( hashedToken ), user.email, 60 * 10 )
+        await this.mailService.sendResetPasswordMail( user.email, link )
+        this.logger.log( `Reset password link sent to ${ user.email }` )
+        this.logger.log( `Reset password token: ${ rowtoken }` )
+        return {
+            message: 'If the email exists, a reset password link has been sent'
+        }
+
+    }
+
+    async resetPassword ( data: ResetPasswordDto )
+    {
+        const hashedToken = hashToken( data.token )
+        const cachedEmail = await this.redisService.get( resetTokenKey( hashedToken ) )
+        if ( !cachedEmail )
+        {
+            throw new HttpException( 'Invalid reset token or expired', 400 )
+        }
+        const user = await this.authRepository.findByEmail( cachedEmail )
+        if ( !user )
+        {
+            throw new HttpException( 'User not found', 404 )
+        }
+
+        const password = await hashPassword( data.newPassword )
+        await this.authRepository.setPassword( password, user.email )
+        await this.redisService.delete( resetTokenKey( hashedToken ) )
+        await this.mailService.sendPasswordChangedMail( user.email )
+        this.logger.log( `User reset password ${ user.email }` )
+        return {
+            message: 'User reset password successfully,Please login with new password'
+        }
+    }
+
+    async getCurrentUser ( userId: string )
+    {
+        const user = await this.authRepository.findById( userId )
+        if ( !user )
+        {
+            throw new HttpException( 'User not found', 404 )
+        }
+        this.logger.log( `User fetched ${ user.email }` )
+        return {
+            message: 'User fetched successfully',
+            user: user
+        }
+    }
 }
