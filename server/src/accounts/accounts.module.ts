51a8a47b591133b1efdbb422b58d09f8e9e3e91e
Ritamnandy <nandyritam519@gmail.com>
2026-09-14 17:44:58 +0530

    update accounts.module.ts

--- server/src/accounts/accounts.module.ts
+++ server/src/accounts/accounts.module.ts
@@ -4,9 +4,10 @@ import { AccountsController } from './accounts.controller.js';
 import { AuthModule } from '../auth/auth.module.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
 import { AccountRepository } from './account.repository.js';
+import { RedisModule } from '../redis/redis.module.js';
 
 @Module( {
-  imports: [ AuthModule, PrismaModule ],
+  imports: [ AuthModule, PrismaModule,RedisModule ],
   controllers: [ AccountsController ],
   providers: [ AccountsService, AccountRepository ],
 } )
