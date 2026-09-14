c74449fb64066cec9e8fe14e3b0783a8a0acd85e
Ritamnandy <nandyritam519@gmail.com>
2026-09-14 14:08:36 +0530

    update create-account.dto.ts

--- server/src/accounts/dto/create-account.dto.ts
+++ server/src/accounts/dto/create-account.dto.ts
@@ -1,1 +1,28 @@
-export class CreateAccountDto {}
+import { IsDate, IsEnum, IsNotEmpty, IsPhoneNumber, IsString, Length } from "class-validator";
+import { AccountType } from "../constants.js";
+
+export class CreateAccountDto
+{
+    @IsNotEmpty()
+    @IsString()
+    @Length( 10, 100 )
+    name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsPhoneNumber( 'IN' )
+    phone: string;
+
+    @IsNotEmpty()
+    @IsDate()
+    dateOfBirth: Date;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsEnum( AccountType )
+    accountType: string;
+}
